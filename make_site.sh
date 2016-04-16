79ef8c363b3ebcb8dca8d486ee58b440d322848e
Clifford Anderson <cliffordmorgananderson@gmail.com>
2016-04-16 18:34:14 -0400

    make subway map default

--- make_site.sh
+++ make_site.sh
@@ -5,4 +5,7 @@ rm -r /var/www/html/MBTA-map/
 echo "Copy new site"
 cp -r MBTA-map /var/www/html/
 
+echo "Make subway map default"
+cp /var/www/html/MBTA-map/subway.html /var/www/html/MBTA-map/index.html
+
 echo "Done"
