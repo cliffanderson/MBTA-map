84622e8e4047647d3a596349f29be70a02098969
Cliff Anderson <cliffordmorgananderson@gmail.com>
2016-04-29 15:03:17 -0400

    add a line

--- parts/js/commuter_lines.js
+++ parts/js/commuter_lines.js
@@ -5,4 +5,5 @@
     var lineOverlay = ['CR-Fairmount', 'CR-Fitchburg', 'CR-Franklin', 'CR-Greenbush',
     'CR-Haverhill', 'CR-Haverhill-Lowell', 'CR-Kingston', 'CR-Lowell', 'CR-Middleborough', 'CR-Needham',
     'CR-Newburyport', 'CR-Newburyport-Rockport', 'CR-Providence', 'CR-Providence-Stoughton', 'CR-Worcester'];
+
     PORT = 1235
